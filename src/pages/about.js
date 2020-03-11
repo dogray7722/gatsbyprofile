0fb891e808a39821f06e092fb83085a5e132daaf
dogray7722 <david.gray61@du.edu>
2020-03-10 22:33:12 -0600

    color changes and some text to work with

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,18 @@ import SEO from '../components/seo'
 const AboutPage = () => (
     <Layout pageTitle="About Dave">
         <SEO title="About"></SEO>
+        <div>
+            <p>Tempor ipsum ut ad cupidatat mollit anim nostrud enim ipsum anim cupidatat ex. Magna anim aliquip occaecat consectetur esse ipsum enim voluptate proident nulla. Lorem consectetur pariatur adipisicing sunt. Irure do deserunt eiusmod sint commodo. Occaecat nisi nisi adipisicing consequat elit ut esse nisi sunt quis quis duis. Anim aute tempor cillum in dolore ipsum id officia. Cupidatat laboris culpa qui esse ullamco.</p>
+
+            <p>Minim aute deserunt eiusmod qui nostrud id elit commodo eu sint velit voluptate. Velit sint incididunt ipsum eiusmod est laboris fugiat duis. Ea minim nostrud aliqua ullamco incididunt laborum cillum adipisicing velit velit</p>
+
+            <p>Nisi laboris qui excepteur mollit non velit ullamco veniam qui quis ad cupidatat incididunt aliqua. Id dolore sit ex velit aliqua laborum et cupidatat ex adipisicing excepteur in. Lorem incididunt nisi excepteur voluptate pariatur consectetur ex dolor pariatur et. Elit tempor voluptate enim incididunt irure cillum non exercitation laborum.</p>
+
+            <p>Occaecat pariatur labore esse sunt quis in voluptate ipsum dolor nostrud non. Ex labore commodo magna elit fugiat minim do esse. Lorem non culpa labore aliqua enim culpa aute id. Nisi magna ipsum reprehenderit sint esse. Adipisicing eiusmod minim nostrud pariatur sint anim.</p>
+
+            <p>Consequat qui minim commodo in aute sint incididunt culpa quis quis qui. Eiusmod do laboris nulla adipisicing ut. Deserunt incididunt non culpa culpa ad non aute aute ipsum laboris nostrud laboris. Irure reprehenderit irure culpa cupidatat. Est consequat adipisicing ipsum adipisicing reprehenderit sunt enim ad eu. Esse ad anim duis occaecat pariatur exercitation nisi est qui ipsum dolor. Occaecat reprehenderit irure incididunt aute nulla do Lorem.</p>
+            
+        </div>
     </Layout>
 )
 
